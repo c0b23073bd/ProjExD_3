92e8d4115b531ae44161074ebb05b5f063a16f88
Sakamoto <c0b23073bd@edu.teu.ac.jp>
2024-10-08 14:28:45 +0900

    練習２

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -157,18 +157,26 @@ def main():
                 # スペースキー押下でBeamクラスのインスタンス生成
                 beam = Beam(bird)###右辺でインスタンスを生成している.ビームが生成された            
         screen.blit(bg_img, [0, 0])
+
+        if bomb is not None:
+            if bird.rct.colliderect(bomb.rct):
+                # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
+                bird.change_img(8, screen)
+                pg.display.update()
+                time.sleep(1)
+                return
         
-        if bird.rct.colliderect(bomb.rct):
-            # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
-            bird.change_img(8, screen)
-            pg.display.update()
-            time.sleep(1)
-            return
+        if beam is not None:
+            if bomb is not None:
+                if beam.rct.colliderect(bomb.rct):#####ビームと爆弾が衝突したら
+                    beam, bomb = None, None
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
         if beam is not None:
-            beam.update(screen)   
+            beam.update(screen)
+        if bomb is not None:
+            bomb.update(screen)  
         bomb.update(screen)
         pg.display.update()
         tmr += 1
